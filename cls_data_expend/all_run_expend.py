8eb52300132db52b15a8130874f65243ce8fc819
SongHanFeng <hanfeng.song@ecarx.com.cn>
2020-05-13 19:06:58 +0800

    data update

--- cls_data_expend/all_run_expend.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# os.system("python -m data_expend.dict_to_json")
-os.system("python -m cls_data_expend.meta_generator")
-os.system("python -m cls_data_expend.json_to_conllx")
-os.system("python -m cls_data_expend.generate_seq")
